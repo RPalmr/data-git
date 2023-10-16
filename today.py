423d4ea5a37d6478e0ba1de3126310c023b6851a
reece palmer <reecelpalmer@gmail.com>
2023-10-16 15:06:20 +0100

    Implement my_buddy_is function

--- today.py
+++ today.py
@@ -10,3 +10,12 @@ def my_name_is():
         str: The name of my github username.
     """
     return "rpalmr"
+
+def my_buddy_is():
+    """
+    Get the name of my buddie's github username.
+
+    Returns:
+        str: The name of my buddie's github username.
+    """
+    return "buddy"
